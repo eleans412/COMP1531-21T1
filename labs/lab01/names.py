8401a6a5b55e55f93fa2894cc03abd3343f29b77
Eleanor Au <eleanorau412@gmail.com>
2021-08-17 17:53:59 +1000

    added lab01

---
+++ labs/lab01/names.py
@@ -0,0 +1,9 @@
+'''
+TODO Complete this file by following the instructions in the lab exercise.
+'''
+
+#print("Name: ")
+
+name = input("Name: ")
+
+print(f"So you call yourself {name} huh?")
---
+++ labs/lab01/strings.py
@@ -0,0 +1,16 @@
+'''
+TODO Complete this file by following the instructions in the lab exercise.
+'''
+
+strings = ['This', 'list', 'is', 'now', 'all', 'together']
+'''sentence = ""
+
+for words in strings:
+    sentence += words + " "
+    
+print(sentence)
+''' 
+sentence = " "
+sentence = sentence.join(strings)
+    
+print(sentence)
